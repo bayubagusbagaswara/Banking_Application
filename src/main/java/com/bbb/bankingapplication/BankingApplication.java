8a74dd6f77ed583bfdd816f853aa70860ea332d3
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-20 10:31:41 +0700

    rename main class

--- src/main/java/com/bbb/bankingapplication/BankingApplication.java
+++ src/main/java/com/bbb/bankingapplication/BankingApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.bbb.bankingapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BankingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BankingApplication.class, args);
 	}
 
 }
--- src/test/java/com/bbb/bankingapplication/BankingApplicationTests.java
+++ src/test/java/com/bbb/bankingapplication/BankingApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.bbb.bankingapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BankingApplicationTests {
 
 	@Test
 	void contextLoads() {
