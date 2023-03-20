b90b0362485f028120caeb757da25ba153ae75cf
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-20 10:55:35 +0700

    add abstract class DateAudit

---
+++ src/main/java/com/bbb/bankingapplication/model/base/DateAudit.java
@@ -0,0 +1,34 @@
+package com.bbb.bankingapplication.model.base;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = { "createdAt", "updatedAt" },
+        allowGetters = true
+)
+public abstract class DateAudit implements Serializable {
+
+    private static final long serialVersionUID = -7645091482655512914L;
+
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
