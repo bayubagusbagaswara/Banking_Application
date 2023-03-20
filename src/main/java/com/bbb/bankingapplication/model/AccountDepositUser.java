ddae63244b8d5cc89793fba872100084928f2947
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-20 14:31:10 +0700

    add model AccountDepositUser

---
+++ src/main/java/com/bbb/bankingapplication/model/AccountDepositUser.java
@@ -0,0 +1,72 @@
+package com.bbb.bankingapplication.model;
+
+import com.bbb.bankingapplication.model.base.UserAudit;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * Ini adalah akun user untuk deposit
+ */
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "account_deposit_user")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountDepositUser extends UserAudit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "disbursement_of_fund")
+    private boolean disbursementOfFund; // pencairan dana
+
+    @Column(name = "system_deposit")
+    private String systemDeposit;
+
+    @Column(name = "deposit_purpose")
+    private String depositPurpose;
+
+    @Column(name = "interest_liquid_acct")
+    private String interestLiquidAcct;
+
+    @Column(name = "maturity_date")
+    private LocalDateTime maturityDate; // tanggal jatuh tempo
+
+    private String depositId;
+
+    private String automaticRollOverOption; // perpanjangan deposit otomatis
+
+    private String mudBilyet;
+
+    private String positionType;
+
+    private String currency;
+
+    private String coCode;
+
+    private String maturityInstrument;
+
+    private String terminal;
+
+    private String struct;
+
+    private String depositNumber;
+
+    private String repayAccount;
+
+    private String nominatedAccount;
+
+    private String branchCode;
+
+    private String branchName;
+
+    private int accountIndex;
+
+    private String cif;
+}
