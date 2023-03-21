46d512fb7c4d1f173295679a0b58bc73c8f911d0
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-21 14:01:55 +0700

    update model CustomerOpenAccount

--- src/main/java/com/bbb/bankingapplication/model/CustomerOpenAccount.java
+++ src/main/java/com/bbb/bankingapplication/model/CustomerOpenAccount.java
@@ -4,9 +4,11 @@ import com.bbb.bankingapplication.model.base.UserAudit;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Data nasabah untuk pembukaan akun / rekening
+ * Apa saja yang dibutuhkan untuk membukan akun nasabah
  */
 @EqualsAndHashCode(callSuper = true)
 @Entity
@@ -22,6 +24,31 @@ public class CustomerOpenAccount extends UserAudit {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "reference_number")
+    private String referenceNumber;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "identification_type")
+    private String identificationType;
+
+    @Column(name = "identification_number")
+    private String identificationNumber;
+
+    @Column(name = "mobile_phone")
+    private String mobilePhone;
+
+    @Column(name = "email_address")
+    private String emailAddress;
+
+    @Column(name = "status")
+    private int status;
+
+    private Date dateOfBirth;
+
+    private String fullName;
+
+    private String branchCode;
+
 }
