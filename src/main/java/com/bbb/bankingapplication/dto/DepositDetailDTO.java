c9ffc42c9a6854b4fa6b2a68a2520466a2f0494a
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-21 15:02:01 +0700

    add dto DepositDetailDTO

---
+++ src/main/java/com/bbb/bankingapplication/dto/DepositDetailDTO.java
@@ -0,0 +1,38 @@
+package com.bbb.bankingapplication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepositDetailDTO {
+
+    private String depositNumber;
+
+    private String branchOffice;
+
+    private String timePeriod;
+
+    private String maturityDate;
+
+    private String systemDeposit;
+
+    private String nisbahBagiHasilNasabah;
+
+    private String nisbahBagiHasilBank;
+
+    private BigDecimal biayaMaterai; // stampDuty
+
+    private BigDecimal depositAmount;
+
+    private String accountAlias;
+
+    private String fromAccountOwnerName;
+
+}
