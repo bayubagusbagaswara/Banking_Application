f2a58a2addb426403209476e758ee4bb6860ace6
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-21 14:02:09 +0700

    update model UserAccount

--- src/main/java/com/bbb/bankingapplication/model/user/UserAccount.java
+++ src/main/java/com/bbb/bankingapplication/model/user/UserAccount.java
@@ -1,10 +1,8 @@
 package com.bbb.bankingapplication.model.user;
 
 import com.bbb.bankingapplication.model.base.UserAudit;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -13,6 +11,7 @@ import javax.persistence.*;
  * Nasabah (user) bisa memilik banyak akun
  * Jadi ini adalah informasi tentang Akun / rekening yang dipilih nasabah
  */
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "user_account")
 @Data
@@ -26,7 +25,28 @@ public class UserAccount extends UserAudit {
     @Column(name = "id")
     private Long id;
 
-    private String name;
+    @JsonIgnore
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_profile_id", referencedColumnName = "id")
+    private UserProfile userProfile;
 
+    @Column(name = "account_number", nullable = false)
+    private String accountNumber;
 
+    @Column(name = "account_status", nullable = false)
+    private Long accountStatus;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "product_type")
+    private String productType;
+
+    @Column(name = "product_code")
+    private String productCode;
+
+    private String currencyCode;
 }
