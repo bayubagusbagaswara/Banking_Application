39f9474417b4aceabac9717c28daac359bcc6341
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-21 14:02:24 +0700

    add service contract DepositService

---
+++ src/main/java/com/bbb/bankingapplication/service/DepositService.java
@@ -0,0 +1,24 @@
+package com.bbb.bankingapplication.service;
+
+public interface DepositService {
+
+    // openingDeposit
+
+    // changeDeposit
+
+    // disbursementOfFounds (Pencairan Data)
+
+    // get list deposit
+
+    // get detail deposit
+
+
+    // generateDataOpenDeposit (ini akan diparsing ke template)
+
+    // receipt (kwitansi)
+    // generateDataReceiptOpenDeposit
+
+    // generateDataTransactionChange
+
+    // generateDataReceiptChange
+}
