ef034e24f4b4346e3c6f2c83b2c51ea80304b996
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-23 09:32:24 +0700

    add method openingDeposit

--- src/main/java/com/bbb/bankingapplication/service/DepositService.java
+++ src/main/java/com/bbb/bankingapplication/service/DepositService.java
@@ -1,8 +1,12 @@
 package com.bbb.bankingapplication.service;
 
+import com.bbb.bankingapplication.dto.OpeningDepositRequest;
+import com.bbb.bankingapplication.dto.OpeningDepositResponse;
+
 public interface DepositService {
 
     // openingDeposit
+    OpeningDepositResponse openingDeposit(OpeningDepositRequest request);
 
     // changeDeposit
 
