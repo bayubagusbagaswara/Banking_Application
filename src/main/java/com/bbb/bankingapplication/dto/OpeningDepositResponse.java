db82b50d07a43a301ebdbaa4e90773181f2f8b26
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-24 08:17:22 +0700

    add payload OpeningDepositResponse

---
+++ src/main/java/com/bbb/bankingapplication/dto/OpeningDepositResponse.java
@@ -0,0 +1,39 @@
+package com.bbb.bankingapplication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OpeningDepositResponse {
+
+    private String id;
+
+    private BigDecimal totalTransaction;
+
+    private String accountNumber;
+
+    private String currency;
+
+    private String accountAlias;
+
+    private String terminal;
+
+    private String depositPurpose;
+
+    private String accountType;
+
+    private String structNumber;
+
+    private String transactionStatus;
+
+    // detail tabungan dto
+    DepositDetailDTO depositDetailDTO;
+
+}
