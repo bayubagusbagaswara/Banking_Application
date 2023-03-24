d465298833230ea12c0245f0c44b77d2ed985f48
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-24 08:17:40 +0700

    add dto UserProfileDTO

---
+++ src/main/java/com/bbb/bankingapplication/dto/UserProfileDTO.java
@@ -0,0 +1,33 @@
+package com.bbb.bankingapplication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileDTO implements Serializable {
+
+    private static final long serialVersionUID = -7405784032778915086L;
+
+    private Long userProfileId;
+
+    private String userId;
+
+    private String fullName;
+
+    private String gender;
+
+    private String citizenship;
+
+    private String birthDate;
+
+    private String birthPlace;
+
+
+}
