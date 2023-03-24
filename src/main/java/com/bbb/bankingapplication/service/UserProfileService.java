e8eee99d1ab62e5e01408a363b04ef1325436a33
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-24 08:18:04 +0700

    update service UserProfileService

--- src/main/java/com/bbb/bankingapplication/service/UserProfileService.java
+++ src/main/java/com/bbb/bankingapplication/service/UserProfileService.java
@@ -1,4 +1,26 @@
 package com.bbb.bankingapplication.service;
 
+import com.bbb.bankingapplication.dto.GetUserDataRequest;
+import com.bbb.bankingapplication.dto.UserProfileDTO;
+
 public interface UserProfileService {
+
+    // get admin profile by id
+    UserProfileDTO getAdminProfileById(GetUserDataRequest request);
+
+    // update user info
+
+    // validate account
+
+    // get all user setting
+
+    // get user profile by cif
+
+    // get user by username
+
+    // get user by id
+
+    // update user profile
+
+    // create account form opening
 }
