3eaac6051ad1f1f3405c36bca08348d28b063991
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-27 14:21:27 +0700

    update several fields UserAccount

--- src/main/java/com/bbb/bankingapplication/model/user/UserAccount.java
+++ src/main/java/com/bbb/bankingapplication/model/user/UserAccount.java
@@ -1,15 +1,31 @@
 package com.bbb.bankingapplication.model.user;
 
 import com.bbb.bankingapplication.model.base.UserAudit;
+import com.bbb.bankingapplication.model.enumerator.AccountStatus;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 /**
  * ini adalah akun (jenis tabungan) milik nasabah
  * Nasabah (user) bisa memilik banyak akun
  * Jadi ini adalah informasi tentang Akun / rekening yang dipilih nasabah
+ * Disinilah user akan pertama kali mendapatkan no rekening (account number)
  */
 @EqualsAndHashCode(callSuper = true)
 @Entity
@@ -31,22 +47,20 @@ public class UserAccount extends UserAudit {
     private UserProfile userProfile;
 
     @Column(name = "account_number", nullable = false)
-    private String accountNumber;
+    private String accountNumber; // no rekening
 
     @Column(name = "account_status", nullable = false)
-    private Long accountStatus;
-
-    @Column(name = "account_type")
-    private String accountType;
+    @Enumerated(EnumType.STRING)
+    private AccountStatus accountStatus; // active or deactivated
 
     @Column(name = "product_name")
-    private String productName;
+    private String productName; // nama produk, misal Easy Wadiah memiliki banyak tipe
 
     @Column(name = "product_type")
-    private String productType;
+    private String productType; // jenis produk
 
     @Column(name = "product_code")
-    private String productCode;
+    private String productCode; // code produk
 
-    private String currencyCode;
+    private String currencyCode; // mata uang yang digunakn untuk produk
 }
