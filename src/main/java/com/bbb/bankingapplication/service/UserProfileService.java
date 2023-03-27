aa53329696402273321d3b88147dc4956f129877
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-27 12:35:10 +0700

    add method contract createUserProfile

--- src/main/java/com/bbb/bankingapplication/service/UserProfileService.java
+++ src/main/java/com/bbb/bankingapplication/service/UserProfileService.java
@@ -1,12 +1,17 @@
 package com.bbb.bankingapplication.service;
 
+import com.bbb.bankingapplication.dto.CreateUserProfileRequest;
+import com.bbb.bankingapplication.dto.CreateUserProfileResponse;
 import com.bbb.bankingapplication.dto.GetUserDataRequest;
-import com.bbb.bankingapplication.dto.UserProfileDTO;
+import com.bbb.bankingapplication.dto.UserInformationDTO;
 
 public interface UserProfileService {
 
+    // create UserProfile
+    CreateUserProfileResponse createUserProfile(CreateUserProfileRequest request);
+
     // get admin profile by id
-    UserProfileDTO getAdminProfileById(GetUserDataRequest request);
+    UserInformationDTO getAdminProfileById(GetUserDataRequest request);
 
     // update user info
 
