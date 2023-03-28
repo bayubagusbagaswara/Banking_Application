c8090333706cac748dcf1296a6508121da5c8d9e
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-28 15:39:21 +0700

    add several field dto CreateUserInformationResponse

--- src/main/java/com/bbb/bankingapplication/dto/CreateUserInformationResponse.java
+++ src/main/java/com/bbb/bankingapplication/dto/CreateUserInformationResponse.java
@@ -1,4 +1,61 @@
 package com.bbb.bankingapplication.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateUserInformationResponse {
+
+    private String id;
+
+    private String userProfileId;
+
+    private String fullName;
+
+    private String shortName;
+
+    private String gender;
+
+    private String maritalStatus;
+
+    private String placeOfBirth;
+
+    private Date dateOfBirth;
+
+    private String citizenship;
+
+    private String residentType;
+
+    private String referralCode;
+
+    private String address;
+
+    private String subDistrict;
+
+    private String city;
+
+    private String province;
+
+    private String branchId;
+
+    private String jobPosition;
+
+    private String cardNumberId;
+
+    private String phoneNumber;
+
+    private String division;
+
+    private String postalCode;
+
+    private String religion;
+
+    private String nik;
 }
