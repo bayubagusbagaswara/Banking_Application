6b003a58387a8b867c3207f3415110f3c764be4b
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-28 14:17:07 +0700

    implement method createUserAccount

--- src/main/java/com/bbb/bankingapplication/service/impl/UserAccountServiceImpl.java
+++ src/main/java/com/bbb/bankingapplication/service/impl/UserAccountServiceImpl.java
@@ -2,22 +2,62 @@ package com.bbb.bankingapplication.service.impl;
 
 import com.bbb.bankingapplication.dto.CreateUserAccountRequest;
 import com.bbb.bankingapplication.dto.CreateUserAccountResponse;
+import com.bbb.bankingapplication.model.enumerator.AccountStatus;
+import com.bbb.bankingapplication.model.user.UserAccount;
+import com.bbb.bankingapplication.model.user.UserProfile;
 import com.bbb.bankingapplication.repository.UserAccountRepository;
+import com.bbb.bankingapplication.repository.UserProfileRepository;
 import com.bbb.bankingapplication.service.UserAccountService;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UserAccountServiceImpl implements UserAccountService {
 
     private final UserAccountRepository userAccountRepository;
+    private final UserProfileRepository userProfileRepository;
 
-    public UserAccountServiceImpl(UserAccountRepository userAccountRepository) {
+    public UserAccountServiceImpl(UserAccountRepository userAccountRepository, UserProfileRepository userProfileRepository) {
         this.userAccountRepository = userAccountRepository;
+        this.userProfileRepository = userProfileRepository;
     }
 
     @Override
-    public CreateUserAccountResponse createUserAccount(CreateUserAccountRequest request) {
-        return null;
+    public CreateUserAccountResponse createUserAccount(Long userProfileId, CreateUserAccountRequest request) {
+        UserProfile userProfile = userProfileRepository.findById(userProfileId)
+                .orElseThrow(() -> new RuntimeException("User profile not found with id : [" + userProfileId + "]"));
+
+        UserAccount userAccount = UserAccount.builder()
+                .userProfile(userProfile)
+                .accountNumber(request.getAccountNumber())
+                .productCode(request.getProductCode())
+                .productName(request.getProductName())
+                .productType(request.getProductType())
+                .currencyCode(request.getCurrencyCode())
+                .build();
+
+        AccountStatus accountStatus = AccountStatus.valueOf(request.getAccountStatus().toUpperCase());
+        userAccount.setAccountStatus(accountStatus);
+
+        LocalDateTime now = LocalDateTime.now();
+        userAccount.setCreatedAt(now);
+        userAccount.setUpdatedAt(now);
+
+        userAccountRepository.save(userAccount);
+
+        return CreateUserAccountResponse.builder()
+                .id(String.valueOf(userAccount.getId()))
+                .userProfileId(String.valueOf(userAccount.getUserProfile().getId()))
+                .accountNumber(userAccount.getAccountNumber())
+                .accountStatus(userAccount.getAccountStatus().name())
+                .productName(userAccount.getProductName())
+                .productType(userAccount.getProductType())
+                .productCode(userAccount.getProductCode())
+                .currencyCode(userAccount.getCurrencyCode())
+                .createdAt(userAccount.getCreatedAt())
+                .updatedAt(userAccount.getUpdatedAt())
+                .build();
     }
 
 }
