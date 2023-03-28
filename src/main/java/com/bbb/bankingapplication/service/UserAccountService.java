f88eccdb77d65fb57581b111af6ce419fcffac32
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-28 09:56:21 +0700

    add commented

--- src/main/java/com/bbb/bankingapplication/service/UserAccountService.java
+++ src/main/java/com/bbb/bankingapplication/service/UserAccountService.java
@@ -3,5 +3,6 @@ package com.bbb.bankingapplication.service;
 public interface UserAccountService {
 
     // cari user profile dulu
+    // if("00".equals(..string..)) atau pengecekan untuk konstanta itu paling kiri, sedangkan variable di kanan
 
 }
