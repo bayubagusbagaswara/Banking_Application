0432a9edd81772380b11647a9d0bef77ac300c18
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-28 14:57:37 +0700

    add util AppConstant

---
+++ src/main/java/com/bbb/bankingapplication/util/AppConstant.java
@@ -0,0 +1,30 @@
+package com.bbb.bankingapplication.util;
+
+import java.util.Locale;
+
+public class AppConstant {
+
+    public static final String USER_BLOCKED_ERROR_CODE = "0025";
+    public static final String ESB = "ESB";
+    public static final String GENERAL_ERROR = "05";
+    public static  final String CHACE_LIST_DEPOSITO ="list_deposito";
+
+    public static  final String CHACE_CHANGE_SYSTEM ="change_sytem";
+    public static final String OPENING_DEPOSITO_SALDO_LESS_MIN = "0136";
+    public static final String CHANGE_DEPOSITO_SALDO_LESS_MIN = "0137";
+    public static final String GENERAL_SOURCE_SYSTEM = "Digital Banking";
+    public static final String CANNOT_PROCESS_DATA = "0052";
+    public static final String SUCCESS = "Success";
+    public static final String FAILED = "Failed";
+    public static final String RC_SUCCESS = "00";
+    public static final String RC_FAILED = "0019";
+    public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    public static final String BIRTHDATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'+07:00'";
+    public static final String FE_FORMAT = "dd MMM yyyy";
+
+    public static final String FE_FORMAT_WIB = "dd MMM yyyy '('HH:mm 'WIB)'";
+    public static final String FE_FORMAT_WITH_TIME = "dd MMM yyyy',' HH:mm:ss";
+
+    public static final Locale LOCALE_IDN = new Locale("id", "ID");
+    public static final Locale LOCALE_ENG = new Locale("en", "US");
+}
