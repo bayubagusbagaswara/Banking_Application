d87ac454e2f1ea217f35f4db254cf429085cccb0
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-29 13:08:22 +0700

    implement method createBank, getBankById, getBankByCode, getAllBankList

--- src/main/java/com/bbb/bankingapplication/service/impl/BankServiceImpl.java
+++ src/main/java/com/bbb/bankingapplication/service/impl/BankServiceImpl.java
@@ -1,8 +1,16 @@
 package com.bbb.bankingapplication.service.impl;
 
+import com.bbb.bankingapplication.dto.bank.BankDTO;
+import com.bbb.bankingapplication.dto.bank.CreateBankRequest;
+import com.bbb.bankingapplication.dto.bank.CreateBankResponse;
+import com.bbb.bankingapplication.model.Bank;
 import com.bbb.bankingapplication.repository.BankRepository;
 import com.bbb.bankingapplication.service.BankService;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BankServiceImpl implements BankService {
@@ -12,4 +20,97 @@ public class BankServiceImpl implements BankService {
     public BankServiceImpl(BankRepository bankRepository) {
         this.bankRepository = bankRepository;
     }
+
+    @Override
+    public CreateBankResponse createBank(CreateBankRequest request) {
+        Bank bank = Bank.builder()
+                .bankCode(request.getBankCode())
+                .areaCode(request.getAreaCode())
+                .bankName(request.getBankName())
+                .bankNetwork(request.getBankNetwork())
+                .bankShortName(request.getBankShortName())
+                .biFastCode(request.getBiFastCode())
+                .ordering(Integer.valueOf(request.getOrdering()))
+                .rtgsCode(request.getRtgsCode())
+                .sknCode(request.getSknCode())
+                .swiftCode(request.getSwiftCode())
+                .build();
+
+        LocalDateTime now = LocalDateTime.now();
+        bank.setCreatedAt(now);
+        bank.setUpdatedAt(now);
+
+        bankRepository.save(bank);
+
+        // kita coba setelah createBank, maka kita balikan hasil save nya
+        // hasil save Bank akan disimpan ke caching
+        // lalu kita coba get bank di caching, coba dengan jeda waktu yang berbeda
+
+        return mapToBankResponse(bank);
+    }
+
+    @Override
+    public BankDTO getBankById(Long id) {
+        Bank bank = bankRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Bank not found with id : [" + id + "]"));
+        return mapToBankDTO(bank);
+    }
+
+    @Override
+    public BankDTO getBankByCode(String bankCode) {
+        Bank bank = bankRepository.findByBankCode(bankCode)
+                .orElseThrow(() -> new RuntimeException("Bank not found with code : [" + bankCode + "]"));
+        return mapToBankDTO(bank);
+    }
+
+    @Override
+    public List<BankDTO> getAllBankList() {
+        List<Bank> bankList = bankRepository.findAll();
+        return mapToBankDTOList(bankList);
+    }
+
+    private static CreateBankResponse mapToBankResponse(Bank bank) {
+        return CreateBankResponse.builder()
+                .id(String.valueOf(bank.getId()))
+                .bankCode(bank.getBankCode())
+                .areaCode(bank.getAreaCode())
+                .bankName(bank.getBankName())
+                .bankNetwork(bank.getBankNetwork())
+                .bankShortName(bank.getBankShortName())
+                .biFastCode(bank.getBiFastCode())
+                .ordering(String.valueOf(bank.getOrdering()))
+                .rtgsCode(bank.getRtgsCode())
+                .sknCode(bank.getSknCode())
+                .swiftCode(bank.getSwiftCode())
+                .createdAt(bank.getCreatedAt())
+                .updatedAt(bank.getUpdatedAt())
+                .build();
+    }
+
+    private static BankDTO mapToBankDTO(Bank bank) {
+        return BankDTO.builder()
+                .id(String.valueOf(bank.getId()))
+                .bankCode(bank.getBankCode())
+                .areaCode(bank.getAreaCode())
+                .bankName(bank.getBankName())
+                .bankNetwork(bank.getBankNetwork())
+                .bankShortName(bank.getBankShortName())
+                .biFastCode(bank.getBiFastCode())
+                .ordering(String.valueOf(bank.getOrdering()))
+                .rtgsCode(bank.getRtgsCode())
+                .sknCode(bank.getSknCode())
+                .swiftCode(bank.getSwiftCode())
+                .createdAt(bank.getCreatedAt())
+                .updatedAt(bank.getUpdatedAt())
+                .createdBy(bank.getCreatedBy())
+                .updatedBy(bank.getUpdatedBy())
+                .build();
+    }
+
+    private static List<BankDTO> mapToBankDTOList(List<Bank> bankList) {
+        return bankList.stream()
+                .map(BankServiceImpl::mapToBankDTO)
+                .collect(Collectors.toList());
+    }
+
 }
