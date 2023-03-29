9bc40ba0766df375840c89f3f28a1a69047b2ade
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-29 09:18:13 +0700

    add method contract transfer

--- src/main/java/com/bbb/bankingapplication/service/TransferService.java
+++ src/main/java/com/bbb/bankingapplication/service/TransferService.java
@@ -1,6 +1,9 @@
 package com.bbb.bankingapplication.service;
 
+import java.math.BigDecimal;
+
 public interface TransferService {
 
     // fromAccount, toAccount, amount
+    void transfer(Long formAccountId, Long toAccountId, BigDecimal amount);
 }
