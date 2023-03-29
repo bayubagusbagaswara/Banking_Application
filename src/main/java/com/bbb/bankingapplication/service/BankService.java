7ea97d97771d5ccc1e8f942c8856231dc6f1a62e
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-29 13:07:00 +0700

    add method createBank, getBankById, getBankByCode, getAllBankList

--- src/main/java/com/bbb/bankingapplication/service/BankService.java
+++ src/main/java/com/bbb/bankingapplication/service/BankService.java
@@ -1,14 +1,27 @@
 package com.bbb.bankingapplication.service;
 
+import com.bbb.bankingapplication.dto.bank.BankDTO;
+import com.bbb.bankingapplication.dto.bank.CreateBankRequest;
+import com.bbb.bankingapplication.dto.bank.CreateBankResponse;
+
+import java.util.List;
+
 public interface BankService {
 
     // create bank
+    CreateBankResponse createBank(CreateBankRequest request);
 
     // get bank
+    BankDTO getBankById(Long id);
+
+    // get bank by code
+    BankDTO getBankByCode(String bankCode);
 
     // get list bank
+    List<BankDTO> getAllBankList();
 
     // update bank
+
 
     // delete bank
 }
