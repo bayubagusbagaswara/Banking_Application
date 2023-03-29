75ec5bb65c81406b38006967a5345f75a3297994
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-29 10:55:05 +0700

    add dto CreateBankRequest

---
+++ src/main/java/com/bbb/bankingapplication/dto/bank/CreateBankRequest.java
@@ -0,0 +1,34 @@
+package com.bbb.bankingapplication.dto.bank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateBankRequest {
+
+    private String bankCode;
+
+    private String areaCode;
+
+    private String bankName;
+
+    private String bankNetwork;
+
+    private String bankShortName;
+
+    private String biFastCode;
+
+    private String ordering;
+
+    private String rtgsCode;
+
+    private String sknCode;
+
+    private String swiftCode;
+
+}
