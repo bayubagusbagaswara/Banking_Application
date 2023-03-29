17b2c61c9ffb019d41a5856bb7bb38943881acab
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-29 10:54:42 +0700

    add service contract BankService

---
+++ src/main/java/com/bbb/bankingapplication/service/BankService.java
@@ -0,0 +1,14 @@
+package com.bbb.bankingapplication.service;
+
+public interface BankService {
+
+    // create bank
+
+    // get bank
+
+    // get list bank
+
+    // update bank
+
+    // delete bank
+}
