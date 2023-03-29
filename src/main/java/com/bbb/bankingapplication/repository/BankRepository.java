856e818090ed5046905f6ed9de7fe05d8b96d924
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-29 12:35:34 +0700

    add method findByBankCode

--- src/main/java/com/bbb/bankingapplication/repository/BankRepository.java
+++ src/main/java/com/bbb/bankingapplication/repository/BankRepository.java
@@ -2,8 +2,14 @@ package com.bbb.bankingapplication.repository;
 
 import com.bbb.bankingapplication.model.Bank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BankRepository extends JpaRepository<Bank, Long> {
+
+    @Query(value = "SELECT * FROM Bank b WHERE b.bankCode = :bankCode", nativeQuery = true)
+    Optional<Bank> findByBankCode(String bankCode);
 }
