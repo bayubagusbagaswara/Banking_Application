401537118113334bebe4f178503a3953bb0ae221
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-03 10:07:01 +0700

    add several field

--- src/main/java/com/bbb/bankingapplication/dto/transfer/TransferResponseDTO.java
+++ src/main/java/com/bbb/bankingapplication/dto/transfer/TransferResponseDTO.java
@@ -5,18 +5,32 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class TransferResponseDTO {
 
-    private String status;
+//    private String status;
+//
+//    private String message;
+//
+//    private String responseCode;
 
-    private String message;
+    private String fromAccountNumber; // nomor rekening asal
+    private String toAccountNumber; // nomor rekening tujuan
 
-    private String responseCode;
+    private String transactionStatus;
 
+    private String localDateTime;
+
+    private BigDecimal amount;
+
+    private BigDecimal adminFee;
+
+    private String note;
 
 
 }
