2770f99124889427db23f06493c25771c7a3d5f4
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-03 13:50:10 +0700

    add DTO UserAccountDTO

---
+++ src/main/java/com/bbb/bankingapplication/dto/UserAccountDTO.java
@@ -0,0 +1,29 @@
+package com.bbb.bankingapplication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserAccountDTO {
+
+    private String id;
+
+    private String userProfileId;
+
+    private String accountNumber;
+
+    private String accountStatus;
+
+    private String productName;
+
+    private String productType;
+
+    private String productCode;
+
+    private String currencyCode;
+}
