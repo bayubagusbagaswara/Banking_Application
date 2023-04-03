958e0916b7e05b8b6a99b625224abe2692649598
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-03 11:26:00 +0700

    add dto UpdateBankResponse

---
+++ src/main/java/com/bbb/bankingapplication/dto/bank/UpdateBankResponse.java
@@ -0,0 +1,41 @@
+package com.bbb.bankingapplication.dto.bank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateBankResponse {
+
+    private String id;
+
+    private String bankCode;
+
+    private String areaCode;
+
+    private String bankName;
+
+    private String bankNetwork;
+
+    private String bankShortName;
+
+    private String biFastCode;
+
+    private String ordering;
+
+    private String rtgsCode;
+
+    private String sknCode;
+
+    private String swiftCode;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
