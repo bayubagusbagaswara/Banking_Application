f1e0f134f0ee66be6b8cc584224fd15bf3456618
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-03 10:07:32 +0700

    add method transferOnlineInquiry, transferOnline

--- src/main/java/com/bbb/bankingapplication/service/TransferService.java
+++ src/main/java/com/bbb/bankingapplication/service/TransferService.java
@@ -1,9 +1,15 @@
 package com.bbb.bankingapplication.service;
 
+import com.bbb.bankingapplication.dto.transfer.TransferRequestDTO;
+import com.bbb.bankingapplication.dto.transfer.TransferResponseDTO;
+
 import java.math.BigDecimal;
 
 public interface TransferService {
 
-    // fromAccount, toAccount, amount
     void transfer(Long fromAccountId, Long toAccountId, BigDecimal amount);
+
+    TransferResponseDTO transferOnlineInquiry(TransferRequestDTO request);
+
+    TransferResponseDTO transferOnline(TransferRequestDTO request);
 }
