3b0f43817773f18a1049dab1ae0f3a908c2486a0
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-03 13:31:08 +0700

    add commented

--- src/main/java/com/bbb/bankingapplication/service/UserAccountService.java
+++ src/main/java/com/bbb/bankingapplication/service/UserAccountService.java
@@ -8,6 +8,7 @@ public interface UserAccountService {
     // cari user profile dulu
     // if("00".equals(..string..)) atau pengecekan untuk konstanta itu paling kiri, sedangkan variable di kanan
 
+    // ini membuat rekening
     CreateUserAccountResponse createUserAccount(Long userProfileId, CreateUserAccountRequest request);
 
 }
