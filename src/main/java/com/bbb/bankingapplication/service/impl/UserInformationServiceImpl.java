abe44b9ae53f9aa7c0e575ea164c8feb7e756d57
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-04 09:09:25 +0700

    add method saveUserInformation and getUserInformationById

--- src/main/java/com/bbb/bankingapplication/service/impl/UserInformationServiceImpl.java
+++ src/main/java/com/bbb/bankingapplication/service/impl/UserInformationServiceImpl.java
@@ -1,6 +1,8 @@
 package com.bbb.bankingapplication.service.impl;
 
+import com.bbb.bankingapplication.dto.CreateUserInformationRequest;
 import com.bbb.bankingapplication.dto.CreateUserInformationResponse;
+import com.bbb.bankingapplication.dto.UserInformationDTO;
 import com.bbb.bankingapplication.repository.UserInformationRepository;
 import com.bbb.bankingapplication.service.UserInformationService;
 import org.springframework.stereotype.Service;
@@ -12,5 +14,15 @@ public class UserInformationServiceImpl implements UserInformationService {
 
     public UserInformationServiceImpl(UserInformationRepository userInformationRepository) {
         this.userInformationRepository = userInformationRepository;
+    }
+
+    @Override
+    public CreateUserInformationResponse saveUserInformation(CreateUserInformationRequest request) {
+        return null;
+    }
+
+    @Override
+    public UserInformationDTO getUserInformationById(Long userInformationId) {
+        return null;
     }
 }
