2972878a0caaa10c07d5b42227efc10e79505cd6
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-04 09:09:09 +0700

    add method contract saveUserInformation and getUserInformationById

--- src/main/java/com/bbb/bankingapplication/service/UserInformationService.java
+++ src/main/java/com/bbb/bankingapplication/service/UserInformationService.java
@@ -1,7 +1,12 @@
 package com.bbb.bankingapplication.service;
 
+import com.bbb.bankingapplication.dto.CreateUserInformationRequest;
+import com.bbb.bankingapplication.dto.CreateUserInformationResponse;
+import com.bbb.bankingapplication.dto.UserInformationDTO;
+
 public interface UserInformationService {
 
-    // disini user akan melengkapi informasi data pribadinya
+    CreateUserInformationResponse saveUserInformation(CreateUserInformationRequest request);
 
+    UserInformationDTO getUserInformationById(Long userInformationId);
 }
