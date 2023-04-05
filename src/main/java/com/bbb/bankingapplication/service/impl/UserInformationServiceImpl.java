17add9e52495d4945f469ac81498cd7653928702
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-05 08:57:59 +0700

    add function mapToUserInformationDTO

--- src/main/java/com/bbb/bankingapplication/service/impl/UserInformationServiceImpl.java
+++ src/main/java/com/bbb/bankingapplication/service/impl/UserInformationServiceImpl.java
@@ -3,7 +3,10 @@ package com.bbb.bankingapplication.service.impl;
 import com.bbb.bankingapplication.dto.CreateUserInformationRequest;
 import com.bbb.bankingapplication.dto.CreateUserInformationResponse;
 import com.bbb.bankingapplication.dto.UserInformationDTO;
+import com.bbb.bankingapplication.model.user.UserInformation;
+import com.bbb.bankingapplication.model.user.UserProfile;
 import com.bbb.bankingapplication.repository.UserInformationRepository;
+import com.bbb.bankingapplication.repository.UserProfileRepository;
 import com.bbb.bankingapplication.service.UserInformationService;
 import org.springframework.stereotype.Service;
 
@@ -11,18 +14,59 @@ import org.springframework.stereotype.Service;
 public class UserInformationServiceImpl implements UserInformationService {
 
     private final UserInformationRepository userInformationRepository;
+    private final UserProfileRepository userProfileRepository;
 
-    public UserInformationServiceImpl(UserInformationRepository userInformationRepository) {
+    public UserInformationServiceImpl(UserInformationRepository userInformationRepository, UserProfileRepository userProfileRepository) {
         this.userInformationRepository = userInformationRepository;
+        this.userProfileRepository = userProfileRepository;
     }
 
     @Override
     public CreateUserInformationResponse saveUserInformation(CreateUserInformationRequest request) {
+        // cari UserProfile by id dulu
+        UserProfile userProfile = userProfileRepository.findById(Long.valueOf(request.getUserProfileId()))
+                .orElseThrow(() -> new RuntimeException("User profile not found with id :[" + request.getUserProfileId() + "]"));
+
+        UserInformation userInformation = UserInformation.builder()
+                .build();
+
+        userInformationRepository.save(userInformation);
         return null;
     }
 
     @Override
     public UserInformationDTO getUserInformationById(Long userInformationId) {
         return null;
+    }
+
+
+    private static CreateUserInformationResponse mapToCreateUserInformationResponse(UserInformation userInformation) {
+        return null;
+    }
+
+    private static UserInformationDTO mapToUserInformationDTO(UserInformation userInformation) {
+        return UserInformationDTO.builder()
+                .userId(String.valueOf(userInformation.getId()))
+                .userProfileId(userInformation.getUserProfile().getId())
+                .fullName(userInformation.getFullName())
+                .shortName(userInformation.getShortName())
+                .gender(userInformation.getGender())
+                .maritalStatus(userInformation.getMaritalStatus())
+                .placeOfBirth(userInformation.getPlaceOfBirth())
+                .dateOfBirth(userInformation.getDateOfBirth())
+                .citizenship(userInformation.getCitizenship())
+                .residentType(userInformation.getResidentType())
+                .address(userInformation.getAddress())
+                .subDistrict(userInformation.getSubDistrict())
+                .city(userInformation.getCity())
+                .province(userInformation.getProvince())
+                .branchId(userInformation.getBranchId())
+                .jobPosition(userInformation.getJobPosition())
+                .phoneHome(userInformation.getPhoneHome())
+                .division(userInformation.getDivision())
+                .postalCode(userInformation.getPostalCode())
+                .religion(userInformation.getReligion())
+                .nik(userInformation.getNik())
+                .build();
     }
 }
