39e0bac4de65b4db3ced80bdeac35ebdc7a7648f
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-05 16:15:07 +0700

    renamed AccountDepositRepository

--- src/main/java/com/bbb/bankingapplication/repository/AccountDepositRepository.java
+++ src/main/java/com/bbb/bankingapplication/repository/AccountDepositRepository.java
@@ -1,9 +1,9 @@
 package com.bbb.bankingapplication.repository;
 
-import com.bbb.bankingapplication.model.user.AccountDepositUser;
+import com.bbb.bankingapplication.model.account.AccountDeposit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountDepositUserRepository extends JpaRepository<AccountDepositUser, Long> {
+public interface AccountDepositRepository extends JpaRepository<AccountDeposit, Long> {
 }
