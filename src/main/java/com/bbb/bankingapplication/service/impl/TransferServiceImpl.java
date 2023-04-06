3ec8f0e0d6c84706f921b8c1bc77fb17c7914fb1
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-06 09:33:37 +0700

    update service method transferOnline

--- src/main/java/com/bbb/bankingapplication/service/impl/TransferServiceImpl.java
+++ src/main/java/com/bbb/bankingapplication/service/impl/TransferServiceImpl.java
@@ -1,5 +1,6 @@
 package com.bbb.bankingapplication.service.impl;
 
+import com.bbb.bankingapplication.dto.deposit.TransferOnlineRequest;
 import com.bbb.bankingapplication.dto.transfer.TransferRequestDTO;
 import com.bbb.bankingapplication.dto.transfer.TransferResponseDTO;
 import com.bbb.bankingapplication.model.user.UserAccount;
@@ -10,6 +11,8 @@ import com.bbb.bankingapplication.service.TransferService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Service
 public class TransferServiceImpl implements TransferService {
@@ -53,15 +56,42 @@ public class TransferServiceImpl implements TransferService {
 
     @Override
     public TransferResponseDTO transferOnline(TransferRequestDTO request) {
-        return null;
+
         // transfer dilakukan ke esb service
         // setelah berhasil, maka esb akan memberikan response
         // lalu response itu akan dimapping ke mappingResponse
         // di mapping response kita setting TransactionStatus
         // code balikan ESB adalah 00 dan 02. 00 -> SUCCESS, 02 -> SUSPECT, selain itu maka FAILED
+
+        // kita create object TransferOnlineRequest, lalu passing data dari TransferRequestDTO ke TransferOnlineRequest
+        TransferOnlineRequest transferOnlineRequest = mapToTransferOnlineRequest(request);
+
+        // response nya apa jika transferOnlineRequest berhasil
+        // disini manual harus menambah dan mengurangi amount masing-masing rekenig
+        // berarti di masing-masing rekening ada data amount
+
+        // lalu mapping response menjadi TransferResponseDTO
+
+        return null;
     }
 
-    // userProfile
-    // userAccount
-    // transactionHistory
+    private static TransferOnlineRequest mapToTransferOnlineRequest(TransferRequestDTO request) {
+        return TransferOnlineRequest.builder()
+                .amount(request.getTransactionAmount().toString())
+                .currency(request.getTransactionCurrency())
+                .transactionDate(generateTransactionDate())
+                .beneficiaryBankCode(request.getToBankCode())
+                .beneficiaryBankName(request.getToBankName())
+                .beneficiaryAccountNumber(request.getToAccountNumber())
+                .beneficiaryAccountName(request.getToAccountOwnerName())
+                .sourceAccountNumber(request.getFromAccountNumber())
+                .sourceAccountName(request.getFromAccountOwnerName())
+                .build();
+    }
+
+    private static String generateTransactionDate() {
+        return (new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")).format(new Date());
+    }
+
+    // map TransactionData to TransactionHistory
 }
