413b8d8801623dd9e72f25b89eabcb7d29609dbc
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-10 09:50:16 +0700

    add service impl AccountProductServiceImpl

---
+++ src/main/java/com/bbb/bankingapplication/service/impl/AccountProductServiceImpl.java
@@ -0,0 +1,19 @@
+package com.bbb.bankingapplication.service.impl;
+
+import com.bbb.bankingapplication.dto.AccountProductDTO;
+import com.bbb.bankingapplication.service.AccountProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountProductServiceImpl implements AccountProductService {
+
+    @Override
+    public AccountProductDTO getAccountProductByName(String productName) {
+        return null;
+    }
+
+    @Override
+    public AccountProductDTO getAccountProductByCode(String productCode) {
+        return null;
+    }
+}
