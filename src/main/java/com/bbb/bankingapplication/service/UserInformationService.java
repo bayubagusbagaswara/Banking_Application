b212524ede7bf6b0879c4af026ebb281b0b8d418
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-10 15:42:11 +0700

    changed UserInformationDTO

--- src/main/java/com/bbb/bankingapplication/service/UserInformationService.java
+++ src/main/java/com/bbb/bankingapplication/service/UserInformationService.java
@@ -1,12 +1,11 @@
 package com.bbb.bankingapplication.service;
 
 import com.bbb.bankingapplication.dto.CreateUserInformationRequest;
-import com.bbb.bankingapplication.dto.CreateUserInformationResponse;
 import com.bbb.bankingapplication.dto.UserInformationDTO;
 
 public interface UserInformationService {
 
-    CreateUserInformationResponse saveUserInformation(CreateUserInformationRequest request);
+    UserInformationDTO saveUserInformation(CreateUserInformationRequest request);
 
     UserInformationDTO getUserInformationById(Long userInformationId);
 }
