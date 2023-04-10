bc422f03a99f240d32a1da535466ca45633b2b5a
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-10 09:44:09 +0700

    update method getAccountProductByName and getAccountProductByCode

--- src/main/java/com/bbb/bankingapplication/service/AccountProductService.java
+++ src/main/java/com/bbb/bankingapplication/service/AccountProductService.java
@@ -4,10 +4,11 @@ import com.bbb.bankingapplication.dto.AccountProductDTO;
 
 public interface AccountProductService {
 
-    // utama disini adalah untuk get
+    // utama disini adalah untuk get (mengambil data Product rekening)
     // misal user memilih membuat account(rekening) dengan product Easy Wadiah
     // jadi kita ambil data Easy Wadiah, kita masukkan ke User
+
     AccountProductDTO getAccountProductByName(String productName);
 
-    void getAccountProductByCode(String productCode);
+    AccountProductDTO getAccountProductByCode(String productCode);
 }
