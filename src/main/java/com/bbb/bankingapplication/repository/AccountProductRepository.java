91327e1eaa9603abad9e046feb654f3935cf93fd
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-10 10:11:19 +0700

    add method findAccountProductByName and findAccountProductByCode

--- src/main/java/com/bbb/bankingapplication/repository/AccountProductRepository.java
+++ src/main/java/com/bbb/bankingapplication/repository/AccountProductRepository.java
@@ -2,8 +2,17 @@ package com.bbb.bankingapplication.repository;
 
 import com.bbb.bankingapplication.model.product.AccountProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AccountProductRepository extends JpaRepository<AccountProduct, Long> {
+
+    @Query(value = "SELECT * FROM AccountProduct ap WHERE ap.productName = :name", nativeQuery = true)
+    Optional<AccountProduct> findAccountProductByName(String name);
+
+    @Query(value = "SELECT * FROM AccountProduct ap WHERE ap.productCode = :code", nativeQuery = true)
+    Optional<AccountProduct> findAccountProductByCode(String code);
 }
