b470fec2c4bc0310b4b634ddfc6de938c22711dd
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-10 10:37:42 +0700

    update several field

--- src/main/java/com/bbb/bankingapplication/dto/AccountProductDTO.java
+++ src/main/java/com/bbb/bankingapplication/dto/AccountProductDTO.java
@@ -5,13 +5,30 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AccountProductDTO {
 
+    private String id;
+
+    private String productCode;
+
     private String productName;
 
     private String productInfo;
+
+    private String productDescription;
+
+    private String productShortDescription;
+
+    private BigDecimal minimumBalance;
+
+    private BigDecimal minimumInitialDeposit;
+
+    private String productFeature;
+
 }
