9cbd1d34fb81522daa22bbc497f0aa3c8ddacd4c
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-10 10:20:47 +0700

    add comment method contract updateAccountProduct and deleteAccountProduct

--- src/main/java/com/bbb/bankingapplication/service/AccountProductService.java
+++ src/main/java/com/bbb/bankingapplication/service/AccountProductService.java
@@ -1,14 +1,24 @@
 package com.bbb.bankingapplication.service;
 
 import com.bbb.bankingapplication.dto.AccountProductDTO;
+import com.bbb.bankingapplication.dto.CreateAccountProductRequest;
 
+/**
+ * Contoh AccountProduct adalah TabunganKu, Easy Wadiah, Easy Mudharabah
+ * utama disini adalah untuk get (mengambil data Product rekening)
+ * misal user memilih membuat account(rekening) dengan product Easy Wadiah
+ * jadi kita ambil data Easy Wadiah, kita masukkan ke User
+ */
 public interface AccountProductService {
 
-    // utama disini adalah untuk get (mengambil data Product rekening)
-    // misal user memilih membuat account(rekening) dengan product Easy Wadiah
-    // jadi kita ambil data Easy Wadiah, kita masukkan ke User
+    // create AccountProduct
+    AccountProductDTO saveAccountProduct(CreateAccountProductRequest request);
 
     AccountProductDTO getAccountProductByName(String productName);
 
     AccountProductDTO getAccountProductByCode(String productCode);
+
+//    AccountProductDTO updateAccountProduct();
+//
+//    void deleteAccountProduct();
 }
