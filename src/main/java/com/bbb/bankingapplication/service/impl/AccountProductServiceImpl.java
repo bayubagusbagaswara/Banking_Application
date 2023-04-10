c8febdd60562a71e2a3ca804e6d7b9f9dae46836
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-10 10:21:12 +0700

    add method saveAccountProduct

--- src/main/java/com/bbb/bankingapplication/service/impl/AccountProductServiceImpl.java
+++ src/main/java/com/bbb/bankingapplication/service/impl/AccountProductServiceImpl.java
@@ -1,11 +1,17 @@
 package com.bbb.bankingapplication.service.impl;
 
 import com.bbb.bankingapplication.dto.AccountProductDTO;
+import com.bbb.bankingapplication.dto.CreateAccountProductRequest;
 import com.bbb.bankingapplication.service.AccountProductService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountProductServiceImpl implements AccountProductService {
+
+    @Override
+    public AccountProductDTO saveAccountProduct(CreateAccountProductRequest request) {
+        return null;
+    }
 
     @Override
     public AccountProductDTO getAccountProductByName(String productName) {
