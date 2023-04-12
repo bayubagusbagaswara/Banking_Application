a1b13293483780d22388a65ca3ead6ef150d2f6c
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-12 10:22:57 +0700

    add class DepositConstant

---
+++ src/main/java/com/bbb/bankingapplication/util/DepositConstant.java
@@ -0,0 +1,36 @@
+package com.bbb.bankingapplication.util;
+
+public class DepositConstant {
+
+    public static final String MODULE_NAME_DEPOSIT = "DEPOSIT";
+    // moduleNameNisbahBank = "NISBAH_BAGI_HASIL_BANK";
+    // moduleNameNisbahNasabah = "NISBAH_BAGI_HASIL_NASABAH";
+    // moduleNameNisbahBank3 = "NISBAH_BAGI_HASIL_BANK_3";
+    // moduleNameNisbahNasabah3 = "NISBAH_BAGI_HASIL_NASABAH_3";
+    // moduleNameNisbahBank6 = "NISBAH_BAGI_HASIL_BANK_6";
+    // moduleNameNisbahNasabah6 = "NISBAH_BAGI_HASIL_NASABAH_6";
+    // nisbahBankDefault = "75";
+    // nisbahNasabahDefault = "25";
+    // nisbahBank3 = "75";
+    // nisbahNasabah3 = "25";
+    // nisbahBank6 = "76";
+    // nisbahNasabah6 = "24";
+    public static final String MODULE_NAME_NISBAH_BANK = "NISBAH_BAGI_HASIL_BANK";
+    public static final String MODULE_NAME_NISBAH_NASABAH = "NISBAH_BAGI_HASIL_NASABAH";
+    public static final String MODULE_NAME_NISBAH_BANK_3 = "NISBAH_BAGI_HASIL_BANK_3";
+    public static final String MODULE_NAME_NISBAH_NASABAH_3 = "NISBAH_BAGI_HASIL_NASABAH_3";
+    public static final String MODULE_NAME_NISBAH_BANK_6 = "NISBAH_BAGI_HASIL_BANK_6";
+    public static final String MODULE_NAME_NISBAH_NASABAH_6 = "NISBAH_BAGI_HASIL_NASABAH_6";
+    public static final String NISBAH_BANK_DEFAULT = "75";
+    public static final String NISBAH_NASABAH_DEFAULT = "25";
+    public static final String NISBAH_BANK_3 = "75";
+    public static final String NISBAH_NASABAH_3 = "25";
+    public static final String NISBAH_BANK_6 = "76";
+    public static final String NISBAH_NASABAH_6 = "24";
+
+    public static final String NISBAH_BAGI_HASIL_BANK_3 = "NISBAH_BAGI_HASIL_BANK_3";
+    public static final String NISBAH_BAGI_HASIL_NASABAH_3 = "NISBAH_BAGI_HASIL_NASABAH_3";
+    public static final String NISBAH_BAGI_HASIL_BANK_6 = "NISBAH_BAGI_HASIL_BANK_6";
+    public static final String NISBAH_BAGI_HASIL_NASABAH_6 = "NISBAH_BAGI_HASIL_NASABAH_6";
+
+}
