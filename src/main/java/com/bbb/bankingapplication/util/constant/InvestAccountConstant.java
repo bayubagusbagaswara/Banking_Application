c7cb0015cbf273744f42d48cbd2e4bb3b80d01fe
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-13 10:45:04 +0700

    add dto InvestAccountConstant

---
+++ src/main/java/com/bbb/bankingapplication/util/constant/InvestAccountConstant.java
@@ -0,0 +1,29 @@
+package com.bbb.bankingapplication.util.constant;
+
+public class InvestAccountConstant {
+
+    public static final String DATA_NOT_FOUND_ERROR_CODE = "0033";
+    public static final String FAILED_OPENING_ACCOUNT = "0133";
+    public static final String SA = "Saving";
+    public static final String FAILED_OPENING_ACCOUNT_NO_REKENING_DORMANT = "0201";
+    public static final String FAILED_OPENING_ACCOUNT_NO_REKENING_BLOKIR = "0202";
+    public static final String GENERAL_ERROR_CODE = "0019";
+    public static final String MODULE_DEPOSITO = "DEPOSITO";
+    public static final String MODULE_ACCOUNT = "ACCOUNT";
+    public static final String NISBAH_BANK= "NISBAH_BAGI_HASIL_BANK";
+    public static final String NISBAH_NASABAH="NISBAH_BAGI_HASIL_NASABAH";
+
+    public static final String NISBAH_BANK_3_BULAN= "NISBAH_BAGI_HASIL_BANK_3";
+    public static final String NISBAH_NASABAH_3_BULAN="NISBAH_BAGI_HASIL_NASABAH_3";
+    public static final String NISBAH_BANK_6_BULAN= "NISBAH_BAGI_HASIL_BANK_6";
+    public static final String NISBAH_NASABAH_6_BULAN="NISBAH_BAGI_HASIL_NASABAH_6";
+
+    public static final String ADMIN_FEE_PENCAIRAN="ADMINFEE_DISBURSMENT";
+    public static final String ADMIN_FEE_UBAH_SYSTEM="ADMINFEE_CHANGE_SYSTEM";
+    public static final String BIAYA_MATERAI="BIAYA_MATERAI";
+
+    public static final String GROSS_REVENUE="GROSS_REVENUE";
+    public static final String IMAGES_LOGO_TNC = "${imageslogo}";
+    public static final String ACCOUNT_ADMIN = "ACCOUNT_ADMIN";
+    public static final String MUD_TENOR ="MUD_TENOR";
+}
