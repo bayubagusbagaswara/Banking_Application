93e20178b38d26b4088ba0aea004cf321fec54d6
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-16 21:17:04 +0700

    update dto UserAccountDTO

--- src/main/java/com/bbb/bankingapplication/dto/UserAccountDTO.java
+++ src/main/java/com/bbb/bankingapplication/dto/UserAccountDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.Instant;
 
 @Data
@@ -29,9 +30,38 @@ public class UserAccountDTO {
 
     private String currencyCode;
 
+    private BigDecimal balance;
+
+    private String accountOwnerName;
+
     private Instant createdAt;
     private Instant updatedAt;
 
     private String createdBy;
     private String updatedBy;
+
+//    @ApiModelProperty("Nomor kartu pada rekening ini")
+//    private String cardNumber;
+//    @ApiModelProperty("Status kartu pada rekening ini")
+//    private String cardStatus;
+//    @ApiModelProperty("Indeks pengurutan untuk rekening ini")
+//    private Integer ordering;
+//    @ApiModelProperty("Penentu rekening ini boleh ditampilkan")
+//    private Boolean isVisible;
+//    @ApiModelProperty("Penentu rekening ini sebagai sumber dana")
+//    private Boolean isSof;
+//    @ApiModelProperty("Informasi tambahan untuk rekening ini")
+//    private String additionalData;
+//    @ApiModelProperty("Aliasing untuk rekening ini")
+//    private String accountAlias;
+//    @ApiModelProperty("Jumlah saldo di rekening ini")
+//    private BigDecimal balance;
+//    @ApiModelProperty("Penentu rekening ini rekening utama")
+//    private Boolean isPrimary;
+//    @ApiModelProperty("Nama pemilik rekening")
+//    private String accountOwnerName;
+//    private String accountLinking;
+//    private int accountIndex;
+//    private Long userProfileId;
+//    private String balanceFormatted;
 }
