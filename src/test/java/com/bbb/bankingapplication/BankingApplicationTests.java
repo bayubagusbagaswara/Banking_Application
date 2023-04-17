214babb2a7f5c4098b240af4aec620f9f9f629e4
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-17 09:33:09 +0700

    update testing

--- src/test/java/com/bbb/bankingapplication/BankingApplicationTests.java
+++ src/test/java/com/bbb/bankingapplication/BankingApplicationTests.java
@@ -6,8 +6,36 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BankingApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static String formattedTransactionDescription(String soul, String totalMoney) {
+		Integer soulInt = Integer.parseInt(soul);
+		StringBuilder resultSoul = new StringBuilder();
+
+		StringBuilder resultTotalMoney = new StringBuilder();
+		StringBuilder resultString = new StringBuilder();
+
+		if (soulInt > 0) {
+			resultSoul.append(soulInt).append("Jiwa"); // 10Jiwa
+		} else {
+			resultSoul.append("0").append("Jiwa"); // 0Jiwa
+		}
+
+		if (totalMoney.length() <= 6) {
+			resultTotalMoney.append("Rp").append(totalMoney, 0, totalMoney.length() - 3).append("Ribu");
+		}
+
+		if (totalMoney.length() > 6 ) {
+			resultTotalMoney.append("Rp").append(totalMoney, 0, totalMoney.length() - 6).append("Juta");
+		}
+
+		// 10JiwaRp450Ribu-ZakatFitrah
+		return String.valueOf(resultString.append(resultSoul).append(resultTotalMoney).append("-").append("ZakatFitrah"));
 	}
 
+	@Test
+	void testFormattedTransactionDescription() {
+		// Format = 1JiwaRp50Ribu-ZakatFitrah
+		String soul = "10";
+		String money = "9000";
+		System.out.println(formattedTransactionDescription(soul, money));
+	}
 }
