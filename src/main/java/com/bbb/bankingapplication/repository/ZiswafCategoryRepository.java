02bf7bbac2fec2f0eeab5d5c7372f394e28592d9
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-18 10:09:49 +0700

    add repository ZiswafCategoryRepository

---
+++ src/main/java/com/bbb/bankingapplication/repository/ZiswafCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.bbb.bankingapplication.repository;
+
+import com.bbb.bankingapplication.model.ZiswafCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ZiswafCategoryRepository extends JpaRepository<ZiswafCategory, Long> {
+}
