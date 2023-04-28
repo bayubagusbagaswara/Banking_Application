859c6b2728e07ee0183f022d57e829e83646f1d2
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-28 09:57:14 +0700

    add service contract AmilService

---
+++ src/main/java/com/bbb/bankingapplication/service/AmilService.java
@@ -0,0 +1,18 @@
+package com.bbb.bankingapplication.service;
+
+import com.bbb.bankingapplication.dto.ziswaf.amil.AmilDTO;
+import com.bbb.bankingapplication.dto.ziswaf.amil.CreateAmilRequest;
+
+public interface AmilService {
+
+    // create amil
+    AmilDTO createAmil(CreateAmilRequest createAmilRequest);
+
+    // get amil
+
+    // get amil list
+
+    // update amil
+
+    // delete amil
+}
