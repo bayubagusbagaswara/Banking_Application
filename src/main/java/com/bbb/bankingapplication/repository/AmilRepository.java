730465f1e9481d5885597c0665f246eb88b52b03
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-28 09:55:58 +0700

    add method findByAmilCode, findByAmilName, findByAmilInitial

--- src/main/java/com/bbb/bankingapplication/repository/AmilRepository.java
+++ src/main/java/com/bbb/bankingapplication/repository/AmilRepository.java
@@ -4,6 +4,14 @@ import com.bbb.bankingapplication.model.ziswaf.Amil;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AmilRepository extends JpaRepository<Amil, Long> {
+
+    Optional<Amil> findByAmilCode(String amilCode);
+
+    Optional<Amil> findByAmilName(String amilName);
+
+    Optional<Amil> findByAmilInitial(String amilInitial);
 }
