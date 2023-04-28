b6cf9ac5402127dd1d1ea4db6d2ef6546ba33bf6
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-28 14:38:33 +0700

    add service contract ZiswafCategoryService

---
+++ src/main/java/com/bbb/bankingapplication/service/ZiswafCategoryService.java
@@ -0,0 +1,16 @@
+package com.bbb.bankingapplication.service;
+
+import com.bbb.bankingapplication.dto.ziswaf.category.CreateZiswafCategoryRequest;
+import com.bbb.bankingapplication.dto.ziswaf.category.ZiswafCategoryDTO;
+
+public interface ZiswafCategoryService {
+
+    // create ZiswafCategory
+    ZiswafCategoryDTO createZiswafCategory(CreateZiswafCategoryRequest request);
+
+    // get ZiswafCategory by code
+
+    // update ZiswafCategory
+
+    // delete category
+}
