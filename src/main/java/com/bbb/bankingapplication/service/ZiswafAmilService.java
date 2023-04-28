7b185bd7d072269c988d3949396b7245e809798e
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-28 08:56:37 +0700

    add service contract ZiswafAmilService

---
+++ src/main/java/com/bbb/bankingapplication/service/ZiswafAmilService.java
@@ -0,0 +1,16 @@
+package com.bbb.bankingapplication.service;
+
+public interface ZiswafAmilService {
+
+    // perlu data amil
+
+    // create amil
+
+    // get amil
+
+    //get list amil
+
+    // update amil
+
+    // delete amil
+}
