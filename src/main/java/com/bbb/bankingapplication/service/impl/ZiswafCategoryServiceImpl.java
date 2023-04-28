7e1f2171de75b21b24532d90157ca2958c3dc0a2
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-28 14:38:54 +0700

    add service impl ZiswafCategoryServiceImpl

---
+++ src/main/java/com/bbb/bankingapplication/service/impl/ZiswafCategoryServiceImpl.java
@@ -0,0 +1,50 @@
+package com.bbb.bankingapplication.service.impl;
+
+import com.bbb.bankingapplication.dto.ziswaf.category.CreateZiswafCategoryRequest;
+import com.bbb.bankingapplication.dto.ziswaf.category.ZiswafCategoryDTO;
+import com.bbb.bankingapplication.model.ziswaf.ZiswafCategory;
+import com.bbb.bankingapplication.repository.ZiswafCategoryRepository;
+import com.bbb.bankingapplication.service.ZiswafCategoryService;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class ZiswafCategoryServiceImpl implements ZiswafCategoryService {
+
+    private final ZiswafCategoryRepository ziswafCategoryRepository;
+
+    public ZiswafCategoryServiceImpl(ZiswafCategoryRepository ziswafCategoryRepository) {
+        this.ziswafCategoryRepository = ziswafCategoryRepository;
+    }
+
+    @Override
+    public ZiswafCategoryDTO createZiswafCategory(CreateZiswafCategoryRequest request) {
+        // check apakah code sudah ada di database
+        ZiswafCategory ziswafCategory = ZiswafCategory.builder()
+                .code(request.getCode())
+                .label(request.getLabel())
+                .description(request.getDescription())
+                .build();
+
+        ziswafCategory.setCreatedBy("SYSTEM");
+        ziswafCategory.setCreatedAt(Instant.now());
+
+        ziswafCategoryRepository.save(ziswafCategory);
+
+        return mapToZiswafCategoryDTO(ziswafCategory);
+    }
+
+    private static ZiswafCategoryDTO mapToZiswafCategoryDTO(ZiswafCategory ziswafCategory) {
+        return ZiswafCategoryDTO.builder()
+                .id(String.valueOf(ziswafCategory.getId()))
+                .code(ziswafCategory.getCode())
+                .label(ziswafCategory.getLabel())
+                .description(ziswafCategory.getDescription())
+                .createdBy(ziswafCategory.getCreatedBy())
+                .createdAt(ziswafCategory.getCreatedAt())
+                .updatedBy(ziswafCategory.getUpdatedBy())
+                .updatedAt(ziswafCategory.getUpdatedAt())
+                .build();
+    }
+}
