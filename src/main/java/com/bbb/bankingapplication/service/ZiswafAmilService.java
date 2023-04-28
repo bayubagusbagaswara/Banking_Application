b5240efdcbcf2d9d590eb239e380e2b1538bf425
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-28 09:57:37 +0700

    update ZiswafAmilService

--- src/main/java/com/bbb/bankingapplication/service/ZiswafAmilService.java
+++ src/main/java/com/bbb/bankingapplication/service/ZiswafAmilService.java
@@ -1,10 +1,14 @@
 package com.bbb.bankingapplication.service;
 
+import com.bbb.bankingapplication.dto.ziswaf.amil.AmilDTO;
+import com.bbb.bankingapplication.dto.ziswaf.amil.CreateAmilRequest;
+
 public interface ZiswafAmilService {
 
     // perlu data amil
 
     // create amil
+    AmilDTO createAmil(CreateAmilRequest createAmilRequest);
 
     // get amil
 
