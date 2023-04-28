9792e29b0550d40b898790c9e516abe5836facfd
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-28 13:25:42 +0700

    add method findByCode

--- src/main/java/com/bbb/bankingapplication/repository/ZiswafCategoryRepository.java
+++ src/main/java/com/bbb/bankingapplication/repository/ZiswafCategoryRepository.java
@@ -4,6 +4,10 @@ import com.bbb.bankingapplication.model.ziswaf.ZiswafCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ZiswafCategoryRepository extends JpaRepository<ZiswafCategory, Long> {
+
+    Optional<ZiswafCategory> findByCode(String code);
 }
