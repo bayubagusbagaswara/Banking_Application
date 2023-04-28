0fe2b94eef82c2adc8b150bf05f6232928292e11
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-28 13:24:50 +0700

    update model ZiswafCategory

--- src/main/java/com/bbb/bankingapplication/model/ziswaf/ZiswafCategory.java
+++ src/main/java/com/bbb/bankingapplication/model/ziswaf/ZiswafCategory.java
@@ -1,14 +1,19 @@
 package com.bbb.bankingapplication.model.ziswaf;
 
 import com.bbb.bankingapplication.model.base.UserAudit;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
-@Table(name = "ziswaf_category")
+@Table(name = "ziswaf_category", uniqueConstraints = {
+        @UniqueConstraint(name = "ziswaf_category_code_unique", columnNames = "code")
+})
 @Data
 @Builder
 @NoArgsConstructor
@@ -17,56 +22,16 @@ public class ZiswafCategory extends UserAudit {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID")
+    @Column(name = "id")
     private Long id;
 
-    @Column(name = "code", length = 255)
+    @Column(name = "code")
     private String code;
 
-    @Column(name = "created_by", length = 255)
-    private String createdBy;
+    @Column(name = "label")
+    private String label;
 
-    @Column(name = "created_date")
-    private Date createdDate;
-
-    @Column(name = "DESCRIPTION", length = 255)
+    @Column(name = "description")
     private String description;
-
-    @Column(name = "image", length = 255)
-    private String image;
-
-    @Column(name = "is_deleted")
-    private boolean isDeleted;
-
-    @Column(name = "modified_date")
-    private Date modifiedDate;
-
-    @Column(name = "modified_by", length = 255)
-    private String modifiedBy;
-
-    @Column(name = "label_en", length = 255)
-    private String nameEn;
-
-    @Column(name = "label_id", length = 255)
-    private String nameId;
-
-    @Column(name = "no_order")
-    private Integer noOrder;
-
-    @Column(name = "parent_category_id")
-    private Long parentCategoryId;
-
-    @Column(name = "service_category", length = 255)
-    private String serviceCategory;
-
-    @Column(name = "tran_code", length = 255)
-    private String tranCode;
-
-    @Column(name = "use_prefix")
-    private Boolean usePrefix;
-
-
-    @Column(name = "status")
-    private Boolean status;
 
 }
