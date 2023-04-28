a7c91d565073c4498f8026689046b5fae27bfa93
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-28 08:56:09 +0700

    update ZiswafZakatService

--- src/main/java/com/bbb/bankingapplication/service/ZiswafZakatService.java
+++ src/main/java/com/bbb/bankingapplication/service/ZiswafZakatService.java
@@ -1,4 +1,7 @@
 package com.bbb.bankingapplication.service;
 
 public interface ZiswafZakatService {
+
+    // melakukan pembayaran, jika sukses pembayaran, maka simpan kedalam transaction history
+    // sebelum melakukan pembayaran, kita inquiry lembaga yg menerima pembayaran kita, lalu kita masukkan jumlah denom yg ingin kita bayarkan
 }
