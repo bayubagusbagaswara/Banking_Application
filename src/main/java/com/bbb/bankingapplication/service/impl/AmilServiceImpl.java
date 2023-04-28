8375feeb7000e6aa5533b92635573e41504c60a0
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-28 09:57:25 +0700

    add service implementation AmilServiceImpl

---
+++ src/main/java/com/bbb/bankingapplication/service/impl/AmilServiceImpl.java
@@ -0,0 +1,14 @@
+package com.bbb.bankingapplication.service.impl;
+
+import com.bbb.bankingapplication.dto.ziswaf.amil.AmilDTO;
+import com.bbb.bankingapplication.dto.ziswaf.amil.CreateAmilRequest;
+import com.bbb.bankingapplication.service.AmilService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AmilServiceImpl implements AmilService {
+    @Override
+    public AmilDTO createAmil(CreateAmilRequest createAmilRequest) {
+        return null;
+    }
+}
