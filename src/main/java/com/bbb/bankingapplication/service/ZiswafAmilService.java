12d69034fbcc4655b3876754ea76a6ba145688be
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-30 18:25:53 +0700

    add method contract createZiswafAmil

--- src/main/java/com/bbb/bankingapplication/service/ZiswafAmilService.java
+++ src/main/java/com/bbb/bankingapplication/service/ZiswafAmilService.java
@@ -2,13 +2,22 @@ package com.bbb.bankingapplication.service;
 
 import com.bbb.bankingapplication.dto.ziswaf.amil.AmilDTO;
 import com.bbb.bankingapplication.dto.ziswaf.amil.CreateAmilRequest;
+import com.bbb.bankingapplication.model.ziswaf.Amil;
+import com.bbb.bankingapplication.model.ziswaf.ZiswafCategory;
 
+/**
+ * Ini sebenarnya table many to many antara Amil dengan Ziswaf Category
+ * Jadi 1 Amil bisa memilik banyak Ziswaf Category, begitu juga sebaliknya
+ */
 public interface ZiswafAmilService {
 
-    // perlu data amil
+    // intinya disini kita hubungkan/relasikan antara Amil dengan Ziswaf Category
+    // tapi kita bikin table ZiswafAmil, data kolom nya adalah gabungan beberapa data antara ZiswafCategory dan Amil
+    // jika kita createZiswafAmil, maka request nya harus juga mengirimkan code ZiswafCategory dan code Amil
 
-    // create amil
-    AmilDTO createAmil(CreateAmilRequest createAmilRequest);
+    void createZiswafAmil(String amilCode, String ziswafCategoryCode);
+
+    // harusnya parameter nya adalah Amil dan Ziswaf Category, lalu kita ambil data-datanya, masukkan ke ZiswafAmil
 
     // get amil
 
